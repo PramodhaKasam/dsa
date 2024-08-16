cff4329ef8d05378ef40dd2efc745b7f2d06cf6e
PramodhaKasam <153810071+PramodhaKasam@users.noreply.github.com>
2024-08-16 14:54:25 +0530

    Create CircularDoublyImplementation.cpp

---
+++ LinkedList/CircularDoubly/CircularDoublyImplementation.cpp
@@ -0,0 +1,65 @@
+// Implementation of Doubly circular linked list
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+    Node(int val)
+    {
+        data= val;
+        next = nullptr;
+        prev = nullptr;
+    };
+};
+
+void createList(Node** head, int n)
+{
+    Node* temp;
+    *head = 0;
+    for(int i=0; i<n; i++)
+    {
+        cout<<"Enter the data for the position "<<i+1<<" : ";
+        int val;
+        cin>>val;
+        Node *newnode = new Node(val);
+        newnode -> next = *head;
+        if(*head == 0)
+        {
+            *head = temp = newnode;
+        }
+        else{
+            temp -> next = newnode;
+            newnode -> prev = temp;
+            temp = newnode;
+        }
+       ( *head) -> prev = newnode;
+    }
+}
+
+void Display(Node* head)
+{
+    Node* temp = head;
+    while(1)
+    {
+        cout<< temp ->data<<" ";
+        temp = temp -> next;
+        if(temp == head)
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of Doubly linked list:";
+    cin>>n;
+    Node *head;
+    createList(&head, n);
+    Display(head);
+    return 0;
+}
