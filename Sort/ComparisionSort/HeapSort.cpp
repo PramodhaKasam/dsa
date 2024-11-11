036b2057d8da984f8d86ab224ba49129fa335bf6
PramodhaKasam <153810071+PramodhaKasam@users.noreply.github.com>
2024-11-11 08:15:14 +0530

    Update HeapSort.cpp

--- Sort/ComparisionSort/HeapSort.cpp
+++ Sort/ComparisionSort/HeapSort.cpp
@@ -1,62 +1,50 @@
-//Heap Sort
+//Heap sort
 #include <bits/stdc++.h>
 using namespace std;
 
-void heapify(vector <int>& arr, int n, int i)
+void heapify(int arr[], int i, int n)
 {
-    int largest = i;
-    int right = 2*i + 2;
-    int left = 2*i + 1;
-    
-    if(left < n && arr[left] > arr[largest])
-        largest = left;
-    if( right < n&& arr[right] > arr[largest])
-        largest = right;
-    
-    if(largest != i)
-    {
-        swap(arr[largest], arr[i]);
-        heapify(arr, n, largest);
-    }
-   
+	int left = 2*i + 1;
+	int right = 2*i +2;
+	
+	int largest = i;
+	
+	if(left < n && arr[left] > arr[i])
+		largest = left;
+	
+	if(right < n && arr[right] > arr[largest])
+		largest = right;
+		
+	if(largest != i)
+	{
+		swap(arr[i], arr[largest]);
+		heapify(arr, largest, n);
+	}
 }
 
-void heapSort(vector <int>& arr, int n)
+void build_heap(int arr[], int n)
 {
-    for(int i=n/2 -1; i>=0; i--)
-    {
-        heapify(arr, n, i);
-    }
-    
-    for(int i=n-1; i>0; i--)
-    {
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
-    }
+	for(int i = n/2 -1 ; i>= 0; i--)
+		heapify(arr, i, n);	
+}
+
+void heapsort(int arr[], int n)
+{
+	build_heap(arr, n);
+	
+	for(int i= n-1; i>=0; i--)
+	{
+		swap(arr[i], arr[0]);
+		heapify(arr, 0, i);
+	}
 }
 
 int main()
 {
-    int n;
-    cin>>n;
-    vector <int> arr(n);
-    for(int i=0; i<n; i++)
-    {
-        cin>>arr[i];
-    }
-    
-    cout<<"Given input: ";
-    for(int i=0; i<n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    
-    heapSort(arr, n); // O(nlogn)
-    cout<<"\nAfter sorting: ";
-    for(int i=0; i<n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    
-    return 0;
+	int arr[] = {1, 2, 8 , 6 , 5, 7};
+	heapsort(arr, 6); //o(nlogn)
+	for(int i=0; i<6; i++)
+		cout<<arr[i]<<" ";
+		
+	return 0;
 }
