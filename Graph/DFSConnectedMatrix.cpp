5a0086e6703c2daf782b64f418fa5303e4527016
PramodhaKasam <153810071+PramodhaKasam@users.noreply.github.com>
2024-11-19 12:05:07 +0530

    Create DFSConnectedMatrix.cpp

---
+++ Graph/DFSConnectedMatrix.cpp
@@ -0,0 +1,49 @@
+//Transpose of connected graph in adjacency matrix
+#include <bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector <vector<int>> &arr, int i, int j)
+{
+	if(arr[i][j] == 0)
+		arr[i][j]++;
+}
+
+void bfs(vector <vector<int>> arr, vector <bool> &visited, int s, int n)
+{
+	visited[s] = true;
+	
+	queue <int> q;
+	q.push(s);
+	
+	while(!q.empty())
+	{
+		int curr = q.front();
+		q.pop();
+		cout<<curr<<" ";
+		
+		for(int i=0; i<n; i++)
+		{
+			if(arr[curr][i] == 1 && visited[i] != true)
+			{
+				visited[i] = true;
+				q.push(i);
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n =5;
+	vector <vector<int>> arr(n, vector<int>(n, 0));
+	
+	addEdge(arr, 0, 4);
+	addEdge(arr, 0, 2);
+	addEdge(arr, 4, 3);
+	addEdge(arr, 2, 1);
+	
+	vector <bool> visited(n, false);
+	bfs(arr, visited, 0, n);
+	
+	return 0;
+}
